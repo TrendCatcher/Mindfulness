7b2eb6a794f2322a5ff779f4fab2f2beb3c33605
TrendCatcher <karleo@kyonggi.ac.kr>
2023-12-06 15:40:34 +0900

    created: Test MemoryMemverRepository code

--- src/test/java/mindfulness/mindfulnessspring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/mindfulness/mindfulnessspring/repository/MemoryMemberRepositoryTest.java
@@ -1,2 +1,16 @@
-package mindfulness.mindfulnessspring.repository;public class MemoryMemberRepositoryTest {
+package mindfulness.mindfulnessspring.repository;
+
+import mindfulness.mindfulnessspring.domain.Member;
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("minfulness");
+
+        repository.save(member);
+    }
 }
