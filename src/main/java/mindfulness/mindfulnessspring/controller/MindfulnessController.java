55de8ccfa12127e1c6a1cf652107e408047d970b
TrendCatcher <karleo@kyonggi.ac.kr>
2024-05-06 11:01:09 +0900

    updated: changed the way to send and receive data to Json type(@ResponseBody)

--- src/main/java/mindfulness/mindfulnessspring/controller/MindfulnessController.java
+++ src/main/java/mindfulness/mindfulnessspring/controller/MindfulnessController.java
@@ -1,38 +1,25 @@
 package mindfulness.mindfulnessspring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MindfulnessController{
-    @GetMapping("mindfulness")
-    public String mindfulness(Model model){
-        model.addAttribute("data", "mindfulness!!");
-        return "mindfulness";
 
-    }
-    @GetMapping("mindfulness-mvc")
-    public String mindfulnessMvc(@RequestParam("name") String name, Model model){
-        model.addAttribute("name", name);
-        return "mindfulness-template";
-    }
-/*API방식*/
+
+
+
+    /*API방식*/
 //    @ResponseBody를 통해 viewResolver를 거치지 않고
 //    그대로 HTTP 바디부분 데이터를 반환
-    @GetMapping("mindfulness-String")
-    @ResponseBody
-    public String mindfulnessString(@RequestParam("name") String name){
-        return "mindfulness"+ name;
-    }
-    @GetMapping("mindfulness-api")
+    @GetMapping("mindfulness-api")      // key-value방식의 JSON형식의 데이터 전달
     @ResponseBody
     public Mindfulness mindfulnessApi(@RequestParam("name") String name){
         Mindfulness mindfulness = new Mindfulness();
         mindfulness.setName(name);
-        return mindfulness;
+        return mindfulness; // 객체를 반환
     }
     static class Mindfulness{
         private String name;
